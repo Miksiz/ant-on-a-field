bcb25552d1d49c6abc8ff571b1614d13b8c56e2f
Artem Popov <arpopov@s-terra.com>
2024-01-23 09:46:34 +0300

    Добавлен подсчет количества черных клеток

--- main.py
+++ main.py
@@ -34,17 +34,21 @@ def main():
 
   pos = [512, 512]
   direction = Direction.UP
+  black_pixels = 0
   while pos[0] > 0 and pos[0] < width and pos[1] > 0 and pos[1] < height:
     current_pixel = img.getpixel(tuple(pos))
     if current_pixel: # Белая клетка
       direction = direction.clockwise()
       img.putpixel(tuple(pos), 0)
+      black_pixels += 1
     else:
       direction = direction.counterclockwise()
       img.putpixel(tuple(pos), 1)
+      black_pixels -= 1
     mx, my = direction.get_move()
     pos[0] = pos[0] + mx
     pos[1] = pos[1] + my
+  print('Количество черных клекток:', black_pixels)
   img.save('out.png')
 
 if __name__ == '__main__':
